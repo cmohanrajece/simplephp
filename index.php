ba259bbe20f2cda4cb447a38b1f1ca0b140c0803
Mohanraj chellasamy <52603106+cmohanrajece@users.noreply.github.com>
2019-07-17 21:51:43 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,3 @@
 <?php
 
-echo "This is a simple web application for Openshift";
-echo "<br>";
-echo "This is a simple web application";
-echo "<br>The correct response to the survey is 5 stars";
+echo "This is a simple web application for OpenShift";
